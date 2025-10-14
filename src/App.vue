0a1554d92ca18bb83050dcb7d07db019d0bca91b
CsDominik21 <csollei.dominik.2021i@bankitatabanya.hu>
2025-10-14 08:11:42 +0200

    MenuBar

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,10 @@
 <script setup>
-
+import MenuBar from './components/MenuBar.vue';
 </script>
 
 <template>
   <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a href="../index.html"><img src="../src/assets/logo_ver2.png" alt="STW logo" title="Mentsd meg az esőerdőket!"></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="../index.html">Főoldal</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Állatok</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Növények</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Interaktív térkép</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Kvíz</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link donate" href="#">Támogass minket</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <MenuBar/>
   </header>
   <main>
 
