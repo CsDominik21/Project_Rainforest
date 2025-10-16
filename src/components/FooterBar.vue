ff068252658948cb78ecd5ddd622c88da08c3a9d
Balogh Máté <94843956+Balgooh@users.noreply.github.com>
2025-10-16 16:03:36 +0200

    Clean code refactor

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,98 +1,105 @@
-<script>
-</script>
-
-
+<script setup></script>
 
 <template>
-    <div class="footer footer-bg zold py-4">
-      <div class="container">
-        <div class="row mt-5">
-          <div class="col-md-4 col-sm-12 pb-5">
-            <h2 class="zold fw-bold">Hogy mindig képben legyél</h2>
-            <h3 class="zold fw-bold display-6">Iratkozz fel a hírlevelünkre!</h3>
-            <button class="btn btn-sub rounded-pill px-4 py-2 mt-3">Feliratkozás!</button>
-          </div>
-  
-          <div class="col-md-3 col-sm-6 offset-md-2">
-            <h5 class="zold fw-bold">Oldalak</h5>
-            <ul class="list-unstyled footer-list">
-              <li><a href="#" class="zold">Élővilág</a></li>
-              <li><a href="#" class="zold">Kvíz</a></li>
-              <li><a href="#" class="zold">Csapatunk</a></li>
-              <li><a href="#" class="zold">Támogatóink</a></li>
-            </ul>
-          </div>
-          <div class="col-md-3 col-sm-6">
-            <h5 class="zold fw-bold">Contact</h5>
-            <ul class="list-unstyled footer-list">
-              <li><a href="#" class="zold"><i class="bi bi-envelope"></i> support@esoorseg.hu</a></li>
-              <li><a href="#" class="zold"><i class="bi bi-phone"></i> +36 30 223 5059</a></li>
-              <li><a href="#" class="zold"><i class="bi bi-house"></i>Tatabánya, Réti u. 1-5, 2800</a></li>
-              <li><a href="#" class="zold"><i class="bi bi-linkedin"></i>Linkedin</a></li>
-            </ul>
-          </div>
+  <div class="footer footer-bg py-4">
+    <div class="container">
+      <div class="row mt-5">
+        <div class="col-md-4 col-sm-12 pb-5">
+          <h2 class="fw-bold">Hogy mindig képben legyél</h2>
+          <h3 class=" fw-bold display-6">Iratkozz fel a hírlevelünkre!</h3>
+          <button class="btn btn-sub rounded-pill px-4 py-2 mt-3">
+            Feliratkozás!
+          </button>
         </div>
 
-        <div class="row justify-content-end mt-4">
-          <div class="col-auto">
-            <a href="#" class="zold me-3"><i class="bi bi-facebook"></i></a>
-            <a href="#" class="zold me-3"><i class="bi bi-whatsapp"></i></a>
-            <a href="#" class="zold me-3"><i class="bi bi-twitter-x"></i></a>
-            <a href="#" class="zold"><i class="bi bi-instagram"></i></a>
-          </div>
+        <div class="col-md-3 col-sm-6 offset-md-2">
+          <h5 class=" fw-bold">Oldalak</h5>
+          <ul class="list-unstyled footer-list">
+            <li><a href="#" >Élővilág</a></li>
+            <li><a href="#" >Kvíz</a></li>
+            <li><a href="#" >Csapatunk</a></li>
+            <li><a href="#" >Támogatóink</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3 col-sm-6">
+          <h5 class=" fw-bold">Contact</h5>
+          <ul class="list-unstyled footer-list">
+            <li>
+              <a href="#"
+                ><i class="bi bi-envelope"></i> support@esoorseg.hu</a
+              >
+            </li>
+            <li>
+              <a href="#" 
+                ><i class="bi bi-phone"></i> +36 30 223 5059</a
+              >
+            </li>
+            <li>
+              <a href="#"
+                ><i class="bi bi-house"></i>Tatabánya, Réti u. 1-5, 2800</a
+              >
+            </li>
+            <li>
+              <a href="#" 
+                ><i class="bi bi-linkedin"></i>Linkedin</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="row justify-content-end mt-4">
+        <div class="col-auto">
+          <a href="#" class=" me-3"><i class="bi bi-facebook"></i></a>
+          <a href="#" class=" me-3"><i class="bi bi-whatsapp"></i></a>
+          <a href="#" class=" me-3"><i class="bi bi-twitter-x"></i></a>
+          <a href="#" ><i class="bi bi-instagram"></i></a>
         </div>
       </div>
     </div>
-  </template>
-  
+  </div>
+</template>
 
-  
-  <style scoped>
-  .footer-bg {
-    background-color: #243524;
-  }
-  
-  .zold {
-    color: #fff;
-  }
-  
-  .btn-sub {
-    background-color: #ecb338;
-    color: #685617;
-    text-transform: uppercase;
-    border: none;
-    font-weight: 700;
-  }
-  
-  .btn-sub:hover {
-    color: #fff;
-    text-transform: uppercase;
-  }
-  
-  a.zold:hover {
-    color: #90ba92;
-    text-decoration: none;
-  }
-  .footer{
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
-  }
-  .container{
-    max-width: 60%;
-  }
-  a{
-    text-decoration: none;
-    margin: 10px !important;
-  }
-  .footer-list li a {
-    display: inline-flex;
-    align-items: center;
-    white-space: nowrap; 
+<style scoped>
+.footer-bg {
+  background-color: #243524;
 }
 
+a, h2, h3, h5, .btn-sub:hover {
+  color: #fff;
+}
 
-  
-  
-  </style>
+.btn-sub {
+  background-color: #ecb338;
+  color: #685617;
+  text-transform: uppercase;
+  border: none;
+  font-weight: 700;
+}
+
+.btn-sub:hover {
+  text-transform: uppercase;
+}
+
+a:hover {
+  color: #90ba92;
+  text-decoration: none;
+}
+.footer {
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+.container {
+  max-width: 60%;
+}
+a {
+  text-decoration: none;
+  margin: 10px !important;
+}
+.footer-list li a {
+  display: inline-flex;
+  align-items: center;
+  white-space: nowrap;
+}
+</style>
