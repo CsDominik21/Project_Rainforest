7228983c2a1e47bd290ca989814050201769c2ec
Balogh Máté <94843956+Balgooh@users.noreply.github.com>
2025-10-17 19:50:15 +0200

    Team.vue v1

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Index from "../pages/Index.vue";
-//import Team from "../pages/Team.vue";
+import Team from "../pages/Team.vue";
 const routes = [
     {
         path: '/',
         component: Index
     },
-    /*{
+    {
         path: '/csapatunk',
         component: Team
-    }*/
+    }
 ];
 
 const router = createRouter({
