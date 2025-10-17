cadfcf6d765e8ec68e36daff9ee3f57ae038a6ef
CsDominik21 <101111675+CsDominik21@users.noreply.github.com>
2025-10-17 22:52:08 +0200

    Roles datas and Role object

---
+++ src/datas/forestRoles.js
@@ -0,0 +1,5 @@
+export const roles = [
+    { icon: 'bi-tree-fill', title: 'Légtisztítás', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec tincidunt nunc, in lacinia risus.', delay: '0.2s' },
+    { icon: 'bi-cloud-rain-fill', title: 'Vízforgalom', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec tincidunt nunc, in lacinia risus.', delay: '0.4s' },
+    { icon: 'bi-bug-fill', title: 'Biodiverzitás', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec tincidunt nunc, in lacinia risus.', delay: '0.6s' }
+];
