4be95ffa731107f0e19062aca5ddbfd1365a2f79
CsDominik21 <101111675+CsDominik21@users.noreply.github.com>
2025-10-26 13:22:16 +0100

    foresttodos text insert

--- src/datas/forestToDos.js
+++ src/datas/forestToDos.js
@@ -1,5 +1,5 @@
 export const toDos = [
-    { icon: 'bi-tree-fill', title: 'Légtisztítás', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec tincidunt nunc, in lacinia risus.', delay: '0.2s' },
-    { icon: 'bi-cloud-rain-fill', title: 'Vízforgalom', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec tincidunt nunc, in lacinia risus.', delay: '0.4s' },
-    { icon: 'bi-bug-fill', title: 'Biodiverzitás', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec tincidunt nunc, in lacinia risus.', delay: '0.6s' }
+    { icon: 'bi-cart-check', title: 'Tudatos vásárlás', description: 'Válassz fenntartható forrásból származó termékeket, kerüld az esőerdőirtáshoz kapcsolódó árukat, és támogasd a környezetbarát márkákat.', delay: '0.2s' },
+    { icon: 'bi-recycle', title: 'Hulladékcsökkentés', description: 'Csökkentsd az egyszer használatos műanyagokat, újrahasznosíts, és törekedj a körforgásos életmódra, hogy kevesebb szemét jusson a természetbe.', delay: '0.4s' },
+    { icon: 'bi-people', title: 'Támogatás', description: 'Csatlakozz környezetvédelmi szervezetekhez, ültess fákat, és terjeszd az ismeretet az esőerdők fontosságáról.', delay: '0.6s' }
 ];
