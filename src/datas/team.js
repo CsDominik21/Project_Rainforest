ab11233cba5ab57047b66f1662d9e0375b11f4e0
Balogh Máté <94843956+Balgooh@users.noreply.github.com>
2025-11-02 20:48:07 +0100

    Member Card tartalom külön fájlba rendezése

---
+++ src/datas/team.js
@@ -0,0 +1,19 @@
+import CsDomi from '../assets/Csdomi.png';
+import BMate from '../assets/Bmate.png';
+
+export const teamMembers = [
+  {
+    name: 'Csöllei Dominik',
+    role: 'Faőrző főnök',
+    email: 'csollei.dominik.2021i@bankitatabanya.hu',
+    phone: '+36 30 123 4312',
+    image: CsDomi
+  },
+  {
+    name: 'Balogh Máté',
+    role: 'Facsemete felügyelő',
+    email: 'balogh.mate.2021i@bankitatabanya.hu',
+    phone: '+36 30 456 7890',
+    image: BMate
+  }
+];
