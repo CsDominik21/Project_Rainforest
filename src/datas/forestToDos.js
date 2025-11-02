e1339c086a7cb806f156f22a770e927da5ac41e4
CsDominik21 <101111675+CsDominik21@users.noreply.github.com>
2025-11-02 21:32:09 +0100

    forestRoles and forestToDos adding one more

--- src/datas/forestToDos.js
+++ src/datas/forestToDos.js
@@ -1,5 +1,6 @@
 export const toDos = [
     { icon: 'bi-cart-check', title: 'Tudatos vásárlás', description: 'Válassz fenntartható forrásból származó termékeket, kerüld az esőerdőirtáshoz kapcsolódó árukat, és támogasd a környezetbarát márkákat.', delay: '0.2s' },
     { icon: 'bi-recycle', title: 'Hulladékcsökkentés', description: 'Csökkentsd az egyszer használatos műanyagokat, újrahasznosíts, és törekedj a körforgásos életmódra, hogy kevesebb szemét jusson a természetbe.', delay: '0.4s' },
-    { icon: 'bi-people', title: 'Támogatás', description: 'Csatlakozz környezetvédelmi szervezetekhez, ültess fákat, és terjeszd az ismeretet az esőerdők fontosságáról.', delay: '0.6s' }
+    { icon: 'bi-people', title: 'Támogatás', description: 'Csatlakozz környezetvédelmi szervezetekhez, ültess fákat, és terjeszd az ismeretet az esőerdők fontosságáról.', delay: '0.6s' },
+    { icon: 'bi-lightbulb', title: 'Energiatakarékosság', description: 'Kapcsold le a feleslegesen égő lámpákat, válts energiatakarékos eszközökre, és hasznosíts megújuló energiaforrásokat, hogy csökkentsd az ökológiai lábnyomodat.', delay: '0.8s' }
 ];
