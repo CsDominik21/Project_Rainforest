55f08629a9b5928f66b19f42d6b4f93891cf8e57
CsDominik21 <101111675+CsDominik21@users.noreply.github.com>
2025-11-03 19:36:56 +0100

    small change in public

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com:CsDominik21/Project_Rainforest.git master:gh-pages
+git push -f git@github.com:CsDominik21/Project_Rainforest.git main:gh-pages
 
 cd -
