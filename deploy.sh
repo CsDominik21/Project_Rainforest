ccfdec31d1dbeea75f4cf2b86e53463d836eb3e6
CsDominik21 <101111675+CsDominik21@users.noreply.github.com>
2025-11-03 19:42:13 +0100

    change

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,9 @@ npm run build
 cd dist
 
 git init
+git checkout -b main
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com:CsDominik21/Project_Rainforest.git master:gh-pages
+git push -f git@github.com:CsDominik21/Project_Rainforest.git main:gh-pages
 
 cd -
